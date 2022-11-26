731601ff9ae900ec9bae298cee53eba5c2af98aa
unknown <marxmaciel@gmail.com>
2022-11-26 02:21:50 -0300

    resgatando dados dos posts do blog

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php 
 include_once("templates/header.php");
 ?>
-    <h1>Hello world!!</h1>
+  <main>
+    <div id="container">
+        <h1>Blog Codar</h1>
+        <p>O seu blog de Programação</p>
+    </div>
+
+    <div id="posts-container">
+        <?php  foreach ($posts as $post) :?>
+            <div class="post-box">
+                <img src="<?= $BASE_URL?>/img/<?= $post['img']?>" alt="<?= $post['title']?>">
+                <h2 class="post-title">
+                    <a href="<?= $BASE_URL?>post.php?id=<?= $post['id'] ?>"> <?= $post['title']?> </a>
+                </h2>
+                <p class="post-description">
+                <?= $post['description']?>
+                </p>
+            </div>
+            <div class="tags-container">
+                <?php  foreach ($post['tags'] as $tag):?>
+
+                    <a href="#"> <?= $tag ?></a>
+                    <?php endforeach; ?>
+            </div>
+            <?php endforeach; ?>
+    </div>
+  </main>
 
     <?php 
 include_once("templates/footer.php");
